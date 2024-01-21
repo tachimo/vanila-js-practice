6404024bae8e56d3b7ef6ae201ea90380e0e3ea0
tachimo <92985370+tachimo@users.noreply.github.com>
2024-01-21 02:42:55 +0000

    Add:追加ボタンの編集JS化

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,27 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグの生成
+  const li = document.createElement("li");
+
+  //divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //pタグの生成
+  const p = document.createElement("p");
+  p.className = "title";
+  p.innerText = inputText;
+
+  //タグ同士の継承
+  div.appendChild(p);
+  li.appendChild(div);
+
+  console.log(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
